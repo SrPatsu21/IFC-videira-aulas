19382c8d211ef8e278e5c3af69ea8251d9781316
paulo <paulosergiopierdona@gmail.com>
2024-09-05 12:02:21 -0300

    thread

--- SistemasOperacionais/AnáliseDeLogDeServidorWeb/main.cpp
+++ SistemasOperacionais/AnáliseDeLogDeServidorWeb/main.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
 #include <fstream>
+#include <thread>
+#include <string>
+#include <list>
+
+#define PACKAGE_SIZE 1000
+
+std::list<std::string>* createPackage(std::fstream* logf)
+{
+    std::string helper;
+    std::list<std::string>* new_package = new std::list<std::string>;
+    int count = 0;
+    while (count < PACKAGE_SIZE && std::getline(*logf, helper))
+    {
+        // std::cout << count << std::endl;
+        new_package->push_back(helper);
+        count++;
+    }
+    if (logf->eof())
+    {
+        logf->close();
+    }
+    return new_package;
+}
+
+void search(int id, std::list<std::string>* package)
+{
+    std::cout << id << " size:" << package->size() << std::endl;
+}
 
 int main()
 {
-    std::ifstream logf;
+    int thread_ids = 0;
+    std::fstream logf;
     logf.open("./access1.log");
-    std::string helper;
+
 
     if (logf.is_open())
     {
-        while (std::getline(logf, helper))
+        while (logf.is_open())
         {
-            std::cout << helper << '\n';
+            thread_ids++;
+            std::thread th(search, thread_ids, createPackage(&logf));
+            th.join();
         }
-        logf.close();
     }
     else
     {
