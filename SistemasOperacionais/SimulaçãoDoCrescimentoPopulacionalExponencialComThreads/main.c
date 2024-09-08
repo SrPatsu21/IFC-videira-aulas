3b2093e59a0fe13c30a83aa43cc2ac9859e84983
SrPatsu <paulosergiopierdona@gmail.com>
2024-09-07 23:09:21 -0300

    imulacap

---
+++ SistemasOperacionais/SimulaçãoDoCrescimentoPopulacionalExponencialComThreads/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <threads.h>
+#include <math.h>
+
+
+
+int main(double P0, double r, int T, int THREADS_AMOUNT)
+{
+    printf("iniciado com: %lf, %lf, %i, %i \n", P0, r, T, THREADS_AMOUNT);
+    return 0;
+}
