f6fccbc1e0fc062e85b7a64126485e38619843f5
SrPatsu <paulosergiopierdona@gmail.com>
2024-10-02 22:21:03 -0300

    start

---
+++ SistemasOperacionais/SimulaçãoDoCrescimentoPopulacionalExponencialComThreadsEPrevençãoDeImpasse/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <threads.h>
+
+#define NULL ((void *)0)
+
+typedef struct Colony
+{
+    int id;
+    double population;
+}COLONY;
