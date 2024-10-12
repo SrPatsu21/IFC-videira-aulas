7e6ec48ebdd286bbcd1319daf90e8653c963e5a5
SrPatsu <paulosergiopierdona@gmail.com>
2024-10-12 13:08:36 -0300

    deadlock

--- SistemasOperacionais/SimulaçãoDoCrescimentoPopulacionalExponencialComThreadsEPrevençãoDeImpasse/growth_simulation.c
+++ SistemasOperacionais/SimulaçãoDoCrescimentoPopulacionalExponencialComThreadsEPrevençãoDeImpasse/growth_simulation.c
@@ -20,12 +20,11 @@ pthread_mutex_t stay_on;
 
 typedef struct Colony
 {
-    int id, p0, t;
+    int id, p0, t, tn, m_space_index, m_resource_index;
     double population, r;
 }COLONY;
 
-//* sub threads to run
-
+//* sub threads number to run
 void subThreads2Run()
 {
   //wait to use
@@ -39,25 +38,77 @@ void subThreads2Run()
 }
 
 //* Space provider
-void lockSpaceProvider()
+int lockSpaceProvider(COLONY* cln)
 {
-  //wait to use
-  pthread_mutex_lock(&space);
+  //* try to find one free
+  for (size_t i = 0; i < space_array_size; i++)
+  {
+    //* verify if is free
+    if (pthread_mutex_trylock(&space[i])==0)
+    {
+      //! already lock
+      // pthread_mutex_lock(&(space[i]));
+      //* save to unlock late
+      cln->m_space_index = i;
+      return 1;
+    }
+  }
+
+  //! not working
+  //* find the small and wait
+  int small_index = 0;
+  for (size_t i = 0; i < space_array_size; i++)
+  {
+    if (&space[small_index].__align > &space[i].__align)
+    {
+      small_index=i;
+    }
+  }
+  pthread_mutex_lock(&space[small_index]);
+  //* save to unlock late
+  cln->m_space_index = small_index;
+  return 2;
 }
-void unlockSpaceProvider()
+void unlockSpaceProvider(COLONY* cln)
 {
-  pthread_mutex_unlock(&space);
+  pthread_mutex_unlock(&space[cln->m_space_index]);
 }
 
 //* Resource provider
-void lockResourceProvider()
+int lockResourceProvider(COLONY* cln)
 {
-  //wait to use
-  pthread_mutex_lock(&resource);
+  //* try to find one free
+  for (size_t i = 0; i < resorce_array_size; i++)
+  {
+    //* verify if is free
+    if (pthread_mutex_trylock(&resource[i])==0)
+    {
+      //! already lock
+      // pthread_mutex_lock(&resource[i]);
+      //* save to unlock late
+      cln->m_resource_index = i;
+      return 1;
+    }
+  }
+
+  //!not working
+  //* find the small and wait
+  int small_index = 0;
+  for (size_t i = 0; i < resorce_array_size; i++)
+  {
+    if (&resource[small_index].__align > &resource[i].__align)
+    {
+      small_index=i;
+    }
+  }
+  pthread_mutex_lock(&resource[small_index]);
+  //* save to unlock late
+  cln->m_resource_index = small_index;
+  return 2;
 }
-void unlockResourceProvider()
+void unlockResourceProvider(COLONY* cln)
 {
-  pthread_mutex_unlock(&resource);
+  pthread_mutex_unlock(&resource[cln->m_resource_index]);
 }
 
 //* bacteria colony
@@ -69,6 +120,9 @@ COLONY* createColony(int id, int p0, int t, double population, double r)
     cln->id = id;
     cln->p0 = p0;
     cln->t = t;
+    cln->tn = 0;
+    cln->m_resource_index = -1;
+    cln->m_space_index = -1;
     cln->population = population;
     cln->r = r;
       return cln;
@@ -87,36 +141,37 @@ void* bacteriaColony(void* colony)
   printf("thread %i start \n", cln->id);
 
   //*threads
-  for (size_t i = 0; i < cln->t; cln->t--)
+  for (cln->tn; cln->tn < cln->t; cln->tn++)
   {
     //* force deadlock
     // this or a semaphore
-    if (*resource.__align)
+    if ((rand()%2) == 1)
     {
       //*lock
-      lockSpaceProvider();
-      lockResourceProvider();
+      lockSpaceProvider(cln);
+      lockResourceProvider(cln);
     }else
     {
       //*lock
-      lockResourceProvider();
-      lockSpaceProvider();
+      lockResourceProvider(cln);
+      lockSpaceProvider(cln);
     }
 
     //* do samething with the resorces
-    //sleep is importante to force a deadlock
-    // usleep(THREAD_WORK_TIME);
-
-    //* unlock
-    unlockSpaceProvider();
-    unlockResourceProvider();
+    // sleep is importante to force a deadlock
+    usleep(THREAD_WORK_TIME);
 
     //*calc
-    cln->population = cln->p0 * exp((cln->r*i));
+    cln->population = cln->p0 * exp((cln->r*cln->tn));
 
-    printf("thread %i grow, time=%i ,now pop=%lf \n", cln->id, ((int)i), cln->population);
+    printf("thread %i grow, time=%i, resorces: s%i r%i ,now pop=%lf \n", cln->id, ((int)cln->tn), cln->m_space_index, cln->m_resource_index, cln->population);
+
+    //* unlock
+    unlockSpaceProvider(cln);
+    unlockResourceProvider(cln);
+
     //* rest
-    // usleep(int(THREAD_WORK_TIME/2));
+    usleep(THREAD_WORK_TIME/2);
   }
 
   printf("thread %i end with pop=%lf \n", cln->id, cln->population);
@@ -126,7 +181,7 @@ void* bacteriaColony(void* colony)
   pthread_exit(NULL);
 }
 
-//** main
+//* main
 int main()
 {
   //* initial pop
@@ -136,15 +191,33 @@ int main()
   //* grow rate
   double r=1.2;
   //* how many threads
-  int colony_number = 200;
+  int colony_number = 15;
 
   //* how many resorces
   space_array_size = 3;
   resorce_array_size = 5;
 
   //* mutex alloc
-  space=malloc(sizeof(pthread_mutex_t)*space_array_size);
-  resource=malloc(sizeof(pthread_mutex_t)*resorce_array_size);
+  space=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*space_array_size);
+  resource=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*resorce_array_size);
+
+  //*mutex init
+  for (size_t i = 0; i < space_array_size; i++)
+  {
+    if (pthread_mutex_init(&(space[i]), NULL) != 0)
+    {
+      printf("\n mutex init has failed\n");
+      return 1;
+    }
+  }
+  for (size_t i = 0; i < resorce_array_size; i++)
+  {
+    if (pthread_mutex_init(&(resource[i]), NULL) != 0)
+    {
+      printf("\n mutex init has failed\n");
+      return 1;
+    }
+  }
 
   printf("Starting with %i colonies \n", colony_number);
 
